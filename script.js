c0638d0e3ced224e8d53b919caeff751d1fcec89
Francisco Lairton Rodolfo Calaca (ALLAN) <75245185+allanrodz@users.noreply.github.com>
2024-04-12 13:16:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+    var acc = document.getElementsByClassName("phase-header");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
